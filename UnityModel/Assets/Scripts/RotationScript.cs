6bff982d8e28ab367bad19e22fb95bae28151552
kongr45gpen <electrovesta@gmail.com>
2018-04-22 01:04:31 +0300

    Add all the updates to the Unity code

--- UnityModel/Assets/Scripts/RotationScript.cs
+++ UnityModel/Assets/Scripts/RotationScript.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System;
 using System.IO.Ports;
+using System.Text;
 using NetMQ.Sockets;
 using NetMQ;
+using UnityEngine.UI;
 
 /*
  * Script to rotate and translate an object in unity according to data received from a ZeroMQ Publisher
@@ -21,7 +23,9 @@ public class RotationScript : MonoBehaviour
     public GameObject target;
 
     //float acc_normalizer_factor = 0.00025f;
-    float gyro_normalizer_factor = 1.0f / 32768.0f;   // 32768 is max value captured during test on imu
+    //float gyro_normalizer_factor = 1.0f / 32768.0f;   // 32768 is max value captured during test on imu
+    float gyro_normalizer_factor = 1.0f / 3268.0f;   // 32768 is max value captured during test on imu
+
 
     float curr_angle_x = 0;
     float curr_angle_y = 0;
@@ -41,10 +45,22 @@ public class RotationScript : MonoBehaviour
     public bool received, calibrated;
 
     //calibrated<> adds the first 40 calibrated values, avgG<> is the average of these first calibrated values that will be reducted so that the system will be calibrated
-    public float calibratedX, calibratedY, calibratedZ, avgGx, avgGy, avgGz;
+    float calibratedX, calibratedY, calibratedZ, avgGx, avgGy, avgGz;
 
     //counter of values added to take the average for calibration purposes
     public int calibrationCounter;
+
+    private int counter;
+
+    public Text rotX;
+    public Text rotY;
+    public Text rotZ;
+    public Text gyroX;
+    public Text gyroY;
+    public Text gyroZ;
+    public Text count;
+    public Text status;
+
 
     Vector3 offset;
 
@@ -96,6 +112,23 @@ public class RotationScript : MonoBehaviour
             // received string is  like  "topic accx accy accz gyrox gyroy gyroz"
             char splitChar = ' ';
             string[] dataRaw = dataString.Split(splitChar);
+            string identifier = dataRaw[1].Substring(0, (dataRaw[1]).Length < 9 ? dataRaw[1].Length : 9);
+
+            if (identifier.Equals("calibrate")) {
+                 calibrated = false;
+                 calibrationCounter = 0;
+                 //reseting angles
+                 curr_angle_x = 0;
+                 curr_angle_y = 0;
+                 curr_angle_z = 0;
+
+                 //reseting calibrating values
+                 calibratedX = 0;
+                 calibratedY = 0;
+                 calibratedZ = 0;
+
+                 return;
+             }
 
             // normalized accelerometer values
             //float ax = float.Parse(dataRaw[1]) * acc_normalizer_factor;
@@ -103,9 +136,9 @@ public class RotationScript : MonoBehaviour
             //float az = float.Parse(dataRaw[3]) * acc_normalizer_factor;
 
             // normalized gyrocope values
-            float gx = float.Parse(dataRaw[4]) * gyro_normalizer_factor;
-            float gy = float.Parse(dataRaw[5]) * gyro_normalizer_factor;
-            float gz = float.Parse(dataRaw[6]) * gyro_normalizer_factor;
+            float gx = float.Parse(dataRaw[1]) * gyro_normalizer_factor;
+            float gy = float.Parse(dataRaw[2]) * gyro_normalizer_factor;
+            float gz = float.Parse(dataRaw[3]) * gyro_normalizer_factor;
 
             //Debug.Log("The data raw of 1 iz: " + gx);
 
@@ -119,6 +152,12 @@ public class RotationScript : MonoBehaviour
             //curr_offset_y += ay;
             //curr_offset_z += 0; // The IMU module have value of z axis of 16600 caused by gravity
 
+            gyroX.text = string.Format("Gyroscope X: {0:N4}", gx);
+            gyroY.text = string.Format("Gyroscope Y: {0:N4}", gy);
+            gyroZ.text = string.Format("Gyroscope Z: {0:N4}", gz);
+
+            status.text = (calibrated) ? "Status: Running" : "Status: Calibrating";
+            count.text = string.Format("Counter: {0:N0}", counter);
 
             // prevent little noise effect
             if (Mathf.Abs(gx) < 0.025f) gx = 0f;
@@ -127,12 +166,17 @@ public class RotationScript : MonoBehaviour
 
             if (calibrated)
             {
+                counter++;
                 curr_angle_x += gx-avgGx;
                 curr_angle_y += gy-avgGy;
                 curr_angle_z += gz-avgGz;
 
                 //if (enableTranslation) target.transform.position = new Vector3(curr_offset_x, curr_offset_y, curr_offset_z);
                 if (enableRotation) target.transform.rotation = Quaternion.Euler(curr_angle_x * factor, curr_angle_y * factor, curr_angle_z * factor);
+
+                rotX.text = string.Format("Rotation X: {0:N4}", curr_angle_x * factor);
+                rotY.text = string.Format("Rotation Y: {0:N4}", curr_angle_y * factor);
+                rotZ.text = string.Format("Rotation Z: {0:N4}", curr_angle_z * factor);
             } else
             {
                 calibrationCounter++;
@@ -147,6 +191,7 @@ public class RotationScript : MonoBehaviour
                     avgGz = calibratedZ / (float)40;
                 }
             }
+
 
         }
 
